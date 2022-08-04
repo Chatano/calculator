dbb9cf0b526bb53622c3661d733319725340ea3b
Chatano <caepradiee@gmail.com>
2022-08-04 18:03:57 -0300

    create display

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,13 +1,14 @@
-import { Container } from "../styles/components/Calculator"
+import { useState } from "react"
+import { Container, Display } from "../styles/components/Calculator"
 
 export const Calculator: React.FC = () => {
+  const [currentResult, setCurrentResult] = useState(0)
+
   return (
     <Container>
-      {/* display */}
-      {/* 
-            buttons 
-        numbers || actions
-      */}
+      <Display>
+        <span>{currentResult}</span>
+      </Display>
     </Container>
   )
 }
--- src/styles/components/Calculator.ts
+++ src/styles/components/Calculator.ts
@@ -5,4 +5,21 @@ export const Container = styled.div`
   width: 320px;
   height: 500px;
   background-color: #ffae70;
+  padding: 12px;
+`;
+
+export const Display = styled.div`
+  background-color: #fdfdfb;
+  border-radius: 8px;
+  padding: 8px 12px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  
+  span {
+    color: #1b1b1b;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 40px;
+  }
 `;
