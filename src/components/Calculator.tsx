ed4d1aa60706d863492d0a933484fcdef78ed6e6
Chatano <caepradiee@gmail.com>
2022-08-04 19:39:28 -0300

    add functionality to type first number

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -12,17 +12,20 @@ import { Button } from "./Button";
 const allNumbers = [...Array.from({length: 9}, (_, i) => i + 1), 0];
 
 export const Calculator: React.FC = () => {
-  const [currentResult, setCurrentResult] = useState(0)
-
+  const [firstNum, setFirstNum] = useState(0)
+  
   const numberButtonClick = (value: number) => {
-
+    setFirstNum(prev => Number(`${prev}${value}`))
   }
 
   return (
     <Container>
       <Display>
-        <span>{currentResult}</span>
+        <span>
+          {firstNum}
+        </span>
       </Display>
+
       <Buttons>
         <TopActions>
           <Button value="CE" />
